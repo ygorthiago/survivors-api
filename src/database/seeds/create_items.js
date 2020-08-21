17a01be19f4b7912ab6b6c1a92500717942aa1a0
ygorthiago <ygorthiagoff@outlook.com>
2020-08-21 01:45:40 -0300

    Add Procfile

--- src/database/seeds/create_items.js
+++ src/database/seeds/create_items.js
@@ -1,8 +1,8 @@
 exports.seed = async function(knex) {
   await knex('items').insert([
-    { name: 'Fiji Water', points: 14, image: 'http://192.168.0.62:3333/items/fiji_water.png' },
-    { name: 'Campbell Soup', points: 12, image: 'http://192.168.0.62:3333/items/campbell_soup.png' },
-    { name: 'First Aid Pouch', points: 10, image: 'http://192.168.0.62:3333/items/fist_aid_pouch.png' },
-    { name: 'AK47', points: 8, image: 'http://192.168.0.62:3333/items/ak47.png' }
+    { name: 'Fiji Water', points: 14, image: 'https://trz-survivors.herokuapp.com/items/fiji_water.png' },
+    { name: 'Campbell Soup', points: 12, image: 'https://trz-survivors.herokuapp.com/items/campbell_soup.png' },
+    { name: 'First Aid Pouch', points: 10, image: 'https://trz-survivors.herokuapp.com/items/fist_aid_pouch.png' },
+    { name: 'AK47', points: 8, image: 'https://trz-survivors.herokuapp.com/items/ak47.png' }
   ]);
 };
